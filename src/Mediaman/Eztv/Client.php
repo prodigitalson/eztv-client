d835f18c1ec7b93e34c78b710a19feed62fe4b18
Ant Cunningham <ant.cunningham@gmail.com>
2014-12-05 15:57:31 -0500

    Adding composer.json and class skeleton.

---
+++ src/Mediaman/Eztv/Client.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Mediaman\Eztv\Client;
+
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Client extends \Goutte\Client
+{
+  
+}
